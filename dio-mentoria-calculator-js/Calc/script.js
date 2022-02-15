0674f8e1d035dc0708ef30d1ce9c162feb19239c
Eder Silva Dev <eder.dasilva2014@gmail.com>
2022-02-15 17:10:58 -0300

    Add files via upload

---
+++ dio-mentoria-calculator-js/Calc/script.js
@@ -0,0 +1,40 @@
+// variaveis
+
+var nome = "Eder";
+console.log(nome);
+
+// function qualquer(){
+//     let nome = "Silva";
+//     console.log(nome)
+// }
+// qualquer();
+
+function alerta(){
+    alert("Alerta invocado ! ")
+}
+alerta();
+
+//const constante = 3.14;
+//console.log(constante)
+
+//let as = false;
+//console.log(typeof(as));
+
+//let name = null;
+//console.log(name)
+
+//let nomeS = "isso é uma String"
+//console.log(typeof(nomeS))
+
+/**TRABALHA COM INDICE */
+// let novoArray = ["eu sou uma String", 33, false, undefined, null];
+// console.log(novoArray)
+
+/**TRABALHA COM PROPRIEDADE */
+// let novoObjeto = {
+//     tipo: "plastico",
+//     cor: "preto",
+//     tamanho: "grande"
+// };
+
+// console.log(novoObjeto)
